45d9899a9919bf35a318441c32dd85833f98052b
MonikaTrajkovska <49168201+MonikaTrajkovska@users.noreply.github.com>
2020-04-27 22:03:28 +0200

    put

---
+++ client/build/precache-manifest.078f3fd6dc21ff141d10d4041b44caaf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb61975381111bd267537fdce176d84e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8245f6891d25a48828dd",
+    "url": "/static/css/main.71b78b40.chunk.css"
+  },
+  {
+    "revision": "f37c2c7d0576e622a88d",
+    "url": "/static/js/2.eaf33276.chunk.js"
+  },
+  {
+    "revision": "ec879bfcc6abcedf81d6091c498a5563",
+    "url": "/static/js/2.eaf33276.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8245f6891d25a48828dd",
+    "url": "/static/js/main.28d2f240.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "92f1cfca0b18b39806521f60acb6817c",
+    "url": "/static/media/Dinningimage.92f1cfca.JPEG"
+  },
+  {
+    "revision": "2872b627dcba78595160c2dcda8531a5",
+    "url": "/static/media/FurniturePicture.2872b627.JPEG"
+  },
+  {
+    "revision": "5b58515787ae63713a4e3a9d57ea92c8",
+    "url": "/static/media/chair.5b585157.JPEG"
+  },
+  {
+    "revision": "e71eae5c99260f69ac1d25f6e5641592",
+    "url": "/static/media/furniture.e71eae5c.JPEG"
+  },
+  {
+    "revision": "dfa40a31f2f450648678c155f88bc76a",
+    "url": "/static/media/table.dfa40a31.JPEG"
+  }
+]);
